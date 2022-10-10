e13f09342b8da6742d6fdb7f1559be466e1fe8e1
FalconSokol1 <jtj6898@gmail.com>
2022-10-10 23:49:57 +0300

    Completed

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+import java.util.*
+fun main(args: Array<String>) {
+    val age1 = 42
+    val age2 = 21
+    //Задание 1 выполнено
+    val avg1 = (age1 + age2) / 2
+    //Задание 2 выполнено
+    val avg2 = (age1.toDouble() + age2.toDouble()) / 2
+    //Задание 3 выполнено
+    val firstName = "Ruslan"
+    val lastName = "Sokolov"
+    //Задание 4 выполнено
+    val fullName = "$firstName $lastName"
+    //Задание 5 выполнено
+    val myDetails = "Привет, меня зовут $fullName"
+    //Задание 6 выполнено
+    val date = Triple(5, 22, 2002)
+    //Задание 7 выполнено
+    var month = date.first
+    var day =  date.second
+    var year = date.third
+    //Задание 8 выполнено
+    println("$month \n$year")
+    //Задание 9 выполнено
+    month = 3
+    val updatedDate = Pair(month, year)
+    println("$updatedDate")
+    //Задание 10 выполнено
+
+
+
+
+
+
+}
+
+
