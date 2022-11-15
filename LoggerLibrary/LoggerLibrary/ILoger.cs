f8f0963caee4ac014c5eb7290627d37a17627a4c
FalconSokol1 <jtj6898@gmail.com>
2022-11-15 12:34:41 +0300

    Долги по 9пр, логи и дз3

---
+++ LoggerLibrary/LoggerLibrary/ILoger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerLibrary
+{
+    internal interface ILoger
+    {
+        Task<string>ReadLog(string path);
+        Task WriteLog(string text);
+
+
+    }
+}
---
+++ dz3/DZ_3/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz3
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public static ObservableCollection<User> GetUser()
+        {
+            var userList = new ObservableCollection<User>()
+            {
+                new User()
+                {
+                    Login = "Sokol",
+                    Password = "2004S"
+                },
+
+                new User()
+                {
+                    Login = "shir",
+                    Password = "qwysz"
+                },
+
+                 new User()
+                {
+                    Login = "shirzxz",
+                    Password = "xsz"
+                },
+            };
+
+            return userList;
+        }
+    }
+}
